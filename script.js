55bd386103a502ee8bfdb880bbfc67d87dfa7878
Tom Tobar <cocdetombomb@gmail.com>
2023-05-11 20:15:07 -0500

    clean up and refactor code for lecture

--- script.js
+++ script.js
@@ -1,72 +1,65 @@
-console.log("Running scripts");
+let allCards = []; // Array for storing all initially fetched data. See the init function to see how this is populated
 
 const renderCards = (dataArray) => {
-  const cardContainer = document.getElementById("card-container");
+  const cardsContainer = document.getElementById("cards-container");
+  cardsContainer.textContent = "";
+
   dataArray.forEach((cardInfo) => {
-    const imgContainer = document.createElement("div");
-    
+    const cardWrapper = document.createElement("div");
+    cardWrapper.className = "card-wrapper";
+
     const img = document.createElement("img");
-    const name = document.createElement("p")
-    const price = document.createElement("p")
-
-    name.textContent = cardInfo.name   
-    price.textContent = "1.00"
-    
-    imgContainer.append(img, name, price)
-
+    img.className = "card-img";
+    img.alt = `${cardInfo.name} image`;
     img.src = cardInfo.image;
 
+    cardWrapper.appendChild(img);
+
     img.addEventListener("dblclick", (event) => {
-        console.log(cardInfo)
-        const pName = document.createElement("p")
-        pName.textContent = cardInfo.name
-        console.log(pName)
-    })
+      const pName = document.createElement("p");
+      pName.textContent = cardInfo.name;
+      console.log(pName);
+    });
 
-
-
-    cardContainer.appendChild(imgContainer);
+    cardsContainer.appendChild(cardWrapper);
   });
 };
 
-
-const fetchData = () => {
-    return fetch("https://botw-compendium.herokuapp.com/api/v2")
-        .then(response => response.json())
-        .then(info => {
-            console.log(info.data)
-            renderCards(info.data.monsters)
-            return info
-        })
-}
+const fetchData = (category = "") => { // This function returns a fetch. Any call to this function will need to handle the returned <Promise> by using a .then() - fetchData().then(data => console.log(data))
+  return fetch(`https://botw-compendium.herokuapp.com/api/v2${category}`)
+    .then(function (response) {
+      return response.json(); // The callback function that is passed to .then() needs to return a value. This code is equivalent to .then(response => response.json())
+    })
+    .then(function (info) {
+      return info.data;
+    });
+};
 
 const addEventListenerToNavLinks = () => {
-    const links = document.querySelectorAll(".link")
-    links.forEach(link => {
-        link.addEventListener("click", (e) => {
-            // 1. Fetch data from json
-            console.log(e)
-            console.log(e.target)
-            debugger
-                // a. Return only images that match the clicked link
-                fetch("https://botw-compendium.herokuapp.com/api/v2")
-                .then(response => response.json())
-                .then(info => {
+  const links = document.querySelectorAll(".link");
+  links.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      const filteredCards = allCards.filter((card) => {  // Filter allCards based on the nav link text that was clicked
+        return card.category.toLowerCase() === e.target.textContent.toLowerCase()
+      });
+      renderCards(filteredCards) // Render the filtered cards array that we created
+    });
+  });
+};
 
-                    renderCards(info.data.monsters)
-                })
-            
-            // 2. Images should remain - only show images that match the clicked link
-            
+const init = () => {
+  addEventListenerToNavLinks(); // add event listeners
 
+  fetchData().then(function (data) { // Send initial fetch for data
+    Object.keys(data).forEach((key) => {
+      if (typeof data[key] === "Array") {        // If data[key] points to an array, then add that array to allCards 
+        allCards = [...allCards, ...data[key]];
+      } else {                                   // Otherwise, data[key] is an object that has top level keys that point to arrays. Grab all of the object's values (arrays) and combine them (.flat() )
+        allCards = [...allCards, ...Object.values(data[key]).flat()]; // .flat() documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat
+      }
+      renderCards(allCards);
+    });
+  });
+};
 
-        })  
-    })
-
-}
-
-addEventListenerToNavLinks()
-fetchData()
-
-
-
+init(); // Initializer function - handles all initial calls to functions that fetch data, populate the DOM and adds event listeners
